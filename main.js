0659ec9be6b90de244080471413a64e70ded77fb
klaus <klaus.foerster@uibk.ac.at>
2025-05-19 20:18:05 +0200

    Template auspacken

---
+++ main.js
@@ -0,0 +1,52 @@
+/* Bike Trail Tirol Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map").setView([ibk.lat, ibk.lng], 9);
+
+// WMTS Hintergrundlayer der eGrundkarte Tirol
+let eGrundkarteTirol = {
+    sommer: L.tileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }),
+    winter: L.tileLayer(
+        "https://wmts.kartetirol.at/gdi_winter/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }),
+    ortho: L.tileLayer("https://wmts.kartetirol.at/gdi_ortho/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }
+    ),
+    nomenklatur: L.tileLayer("https://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
+        pane: "overlayPane",
+    })
+}
+
+// / Layer control mit eGrundkarte Tirol und Standardlayern
+L.control.layers({
+    "eGrundkarte Tirol Sommer": L.layerGroup([
+        eGrundkarteTirol.sommer,
+        eGrundkarteTirol.nomenklatur
+    ]).addTo(map),
+    "eGrundkarte Tirol Winter": L.layerGroup([
+        eGrundkarteTirol.winter,
+        eGrundkarteTirol.nomenklatur
+    ]),
+    "eGrundkarte Tirol Orthofoto": L.layerGroup([
+        eGrundkarteTirol.ortho,
+        eGrundkarteTirol.nomenklatur,
+    ]),
+    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
