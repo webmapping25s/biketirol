490767f47d54cbdb1d64252325a445b234ba9578
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-22 16:57:37 +0200

    auf Wechsel in Pulldown reagieren

--- main.js
+++ main.js
@@ -67,3 +67,9 @@ for (let etappe of ETAPPEN) {
         <option ${selected} value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.titel}</option>
     `;
 }
+
+// auf Wechsel in Pulldown reagieren
+pulldown.onchange = function(evt) {
+    //console.log(evt.target.value);
+    window.location.href = `https://${evt.target.value}.github.io/biketirol`;
+}
